2e945344a591b58e7d5f9934fe896936013e1957
Daniel Silverio <danielsr4647@gmail.com>
2023-12-26 16:59:31 -0300

    Python - Reducing Code

--- calculator.py
+++ calculator.py
@@ -1,86 +1,32 @@
-from typing import Union, Optional
-
 import sys
-import re
-
-result: Union[int, float] = 0
-
-def sum(adder: Union[int, float]) -> Union[int, float]:
-    global result
-    result += adder
-
-    return result
-
-def sub(subtractor: Union[int, float]) -> Union[int, float]:
-    global result
-    result -= subtractor
-
-    return result
-
-def mul(multiplier: Union[int, float]) -> Union[int, float]:
-    global result
-    result *= multiplier
-
-    return result  
-
-def div(divider: Union[int, float]) -> Union[int, float]:
-    global result
-    result /= divider
-
-    return result
-
-
-def findNumber(calculation: str) -> re.Match:
-    return re.match(r"[0-9]{1,}", calculation)
-
-def findSignal(calculation: str) -> re.Match:
-    return re.match(r"[\+\-\*\/]", calculation)
 
 
 def main() -> int:
-    global result
+    result: float = 0.0
 
-    calculation: str = input("Type the Calculation: ").strip()
-    calculate: bool = False
+    calculation: str = input("Type the Calculation: ").strip().split(' ')
+    if calculation[0].replace('.', '').isnumeric():
+        result=float(calculation[0])
+        calculation.pop(0)
+        
+    for i in range(len(calculation)):
+        if calculation[i] in '+-':
+            # Check string of next index is float or int
+            if calculation[i+1].replace('.', '').isnumeric():
+                result += float(calculation[i] + calculation[i+1])
+                i += 1
 
-    functions = { '+': sum, '-': sub, '*': mul, '/': div }
+        elif calculation[i] == '*':
+            if calculation[i+1].replace('.', '').isnumeric():
+                result *= float(calculation[i+1])
+                i = 1
 
-    while calculation != '':
-        number = findNumber(calculation)
+        elif calculation[i] == '/':
+            if calculation[i+1].replace('.', '').isnumeric():
+                result /= float(calculation[i+1])
+                i = 1
 
-        if number:
-            start_num, stop_num = number.span()
-            calculation = calculation[stop_num:].strip()
+    print(result)
 
-            number = float(number.string[start_num:stop_num])
-
-        signal = findSignal(calculation)
-
-        if signal:
-            start_sig, stop_sig = signal.span()
-            calculation = calculation[stop_sig:].strip()
-
-            signal = signal.string[start_sig:stop_sig]
-
-            if type(number) == float:
-                result = number
-                number = findNumber(calculation)
-
-                if number:
-                    start_num, stop_num = number.span()
-                    calculation = calculation[stop_num:].strip()
-
-                    number = float(number.string[start_num:stop_num])
-
-                    functions[signal](number)
-
-
-    if str(result).split('.')[1] == '0':
-        result = int(result)
-
-    print("It's equal ", result)
-
-    return 0
-
-
-if __name__ == "__main__": sys.exit(main())
+if __name__ == "__main__":
+    sys.exit(main())
