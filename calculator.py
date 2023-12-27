74e8ccc2e293f2b2bf39f287c17400f4c7cd1ba9
Daniel Silverio <danielsr4647@gmail.com>
2023-12-27 11:48:13 -0300

    Python - Added the possbility of calculate with spaces

--- calculator.py
+++ calculator.py
@@ -1,30 +1,48 @@
+import re
 import sys
 
 
 def main() -> int:
-    result: float = 0.0
+    result = 0.0
 
-    calculation: str = input("Type the Calculation: ").strip().split(' ')
+    calculation: list = input("Type the Calculation: ").strip().split(' ')
+    buff_calc = []
+
+    for item in calculation:
+        while item != '':
+            number = re.match("[0-9]{1,}", item)
+            if number:
+                buff_calc.append(item[number.span()[0]:number.span()[1]])
+                item = item[number.span()[1]:]
+
+            sign = re.match("[\+\-\*\/]", item)
+            if sign:
+                buff_calc.append(item[sign.span()[0]:sign.span()[1]])
+                item = item[sign.span()[1]:]
+
+    calculation = buff_calc
+    del buff_calc
+
+    signs = ['+','-','*','/']
+    # Check string of next index is a number
     if calculation[0].replace('.', '').isnumeric():
         result=float(calculation[0])
         calculation.pop(0)
         
     for i in range(len(calculation)):
         if calculation[i] in '+-':
-            # Check string of next index is float or int
-            if calculation[i+1].replace('.', '').isnumeric():
-                result += float(calculation[i] + calculation[i+1])
-                i += 1
+            i += 1
 
-        elif calculation[i] == '*':
-            if calculation[i+1].replace('.', '').isnumeric():
-                result *= float(calculation[i+1])
-                i += 1
+            if calculation[i].replace('.', '').isnumeric():
+                result += float(calculation[i-1]+calculation[i])
 
-        elif calculation[i] == '/':
-            if calculation[i+1].replace('.', '').isnumeric():
-                result /= float(calculation[i+1])
-                i += 1
+        elif calculation[i] in '*/':
+            i += 1
+
+            if calculation[i].replace('.', '').isnumeric():
+                number = float(calculation[i])
+                if calculation[i-1] == '*': result *= number
+                else: result /= number
 
     print(result)
 
