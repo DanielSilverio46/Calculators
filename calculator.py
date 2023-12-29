24acaa640a8817603f3a13bac7211d660128642f
Daniel Silverio <danielsr4647@gmail.com>
2023-12-29 10:45:03 -0300

    Python - Add order of calculations

--- calculator.py
+++ calculator.py
@@ -1,50 +1,84 @@
 import re
 import sys
 
+from typing import Union, List
 
-def main() -> int:
+def sumStrToFloat(number: Union[str, float, int], numberWithSign: Union[str, float, int]) -> float:
+    '''
+    :number: Can be a string, float or int 
+    :numberWithSignal: Can be a string, float or int number, an it can has a signal for example: "+1" or "-1"
+    '''
+
+    return float(number) + float(numberWithSign)
+
+def mulStrToFloat(number: Union[float, int], number2: Union[str, float, int]) -> float:
+    return float(number) * float(number2)
+
+def divStringtoFloat(number: Union[float, int], number2: Union[str, float, int]) -> float:
+    return float(number) / float(number2)
+
+def main(argv: List[int]) -> int:
+    debug = False
+    for string in argv:
+        if string == "debug=true": debug=True
+
+    func = {
+        '+': sumStrToFloat,
+        '-': sumStrToFloat,
+        '*': mulStrToFloat,
+        '/': divStringtoFloat }
+
     result = 0.0
-
     calculation: list = input("Type the Calculation: ").strip().split(' ')
+
     buff_calc = []
-
+                    # float                # int        # signals
+    reStrings = [r"[0-9]{1,}\.[0-9]{1,}",  r"[0-9]{1,}", r"[\+\-\*\/]"]
     for item in calculation:
-        while item != '':
-            number = re.match("[0-9]{1,}", item)
-            if number:
-                buff_calc.append(item[number.span()[0]:number.span()[1]])
-                item = item[number.span()[1]:]
+        i=0 # Start with float 
 
-            sign = re.match("[\+\-\*\/]", item)
-            if sign:
-                buff_calc.append(item[sign.span()[0]:sign.span()[1]])
-                item = item[sign.span()[1]:]
+        while item != '':
+            try:
+                match = re.match(reStrings[i], item)
+
+                buff_calc.append(item[match.span()[0]:match.span()[1]])
+                item = item[match.span()[1]:]
+
+                i+=1
+                if i>2: i=0 
+
+            except AttributeError:
+                i+=1
+                if i>2: i=0
 
     calculation = buff_calc
-    del buff_calc
+    del buff_calc, reStrings
 
-    signs = ['+','-','*','/']
-    # Check string of next index is a number
-    if calculation[0].replace('.', '').isnumeric():
-        result=float(calculation[0])
-        calculation.pop(0)
-        
-    for i in range(len(calculation)):
-        if calculation[i] in '+-':
-            i += 1
+    OrderSigns = ['/','*','-','+']
+   
+    while len(calculation) != 1:
+        if debug: print(calculation)
 
-            if calculation[i].replace('.', '').isnumeric():
-                result += float(calculation[i-1]+calculation[i])
+        signal, i = None, 0
+        while not signal:
+            try: signal = calculation.index(OrderSigns[i])
+            except ValueError: i+=1
 
-        elif calculation[i] in '*/':
-            i += 1
+        calculation[signal-1] = func[calculation[signal]] (
+            calculation[signal-1],
+            calculation[signal+1] )
+        calculation.pop(signal)
+        calculation.pop(signal)
+        # One example of what's going on here
+        # ['11', '+', '11'}
+        #    ^____^____^
+        #         |
+        #        22
+        # [22, '+', '11'] -> two pop is called -> [22]
 
-            if calculation[i].replace('.', '').isnumeric():
-                number = float(calculation[i])
-                if calculation[i-1] == '*': result *= number
-                else: result /= number
+    result = calculation[0]
 
-    print(result)
+    # print result with float numbers if it has a value otherwise print at format of int
+    print(f"{result:.0F}") if str(result).split('.')[1]=='0' else print(result)
 
-if __name__ == "__main__":
-    sys.exit(main())
+if __name__ == "__main__": sys.exit(main(sys.argv))
